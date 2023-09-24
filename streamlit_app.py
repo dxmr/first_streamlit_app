b73e68fc02a9425841a9e0b8a3fc20cb036f6180
Deepak Kumar <mailtodeepak321@gmail.com>
2023-09-24 15:31:13 +0530

    Breakfast Menu Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit
 
-streamlit.title("Hi Tittle")
+streamlit.title("My Mom's New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boilded Free-Range Egg")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boilded Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
