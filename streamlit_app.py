8d8dfb74733175a1758db98e54f5fc513beebe38
Deepak Kumar <mailtodeepak321@gmail.com>
2023-09-25 11:48:00 +0530

    Fruity advice updated.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 streamlit.title("My Mom's New Healthy Diner")
 
-streamlit.header("Breakfast Favorites")
+"""streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boilded Free-Range Egg")
@@ -17,4 +17,9 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+"""
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response)
 
